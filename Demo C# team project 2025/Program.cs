fa41736818bc316b5d86e4b048c3e6ca15e0518c
Tetyana-K <tanya.kyryk@gmail.com>
2025-11-03 19:00:28 +0200

    add sum of numbers 1..5

--- Demo C# team project 2025/Program.cs
+++ Demo C# team project 2025/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, team!");
 Console.WriteLine("Lets start!");
+int sum = 0;
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Count: {i}");
+    sum += i;
+}
+Console.WriteLine(sum);
