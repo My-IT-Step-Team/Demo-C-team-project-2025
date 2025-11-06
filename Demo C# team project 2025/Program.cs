9684357ab571481282f769ba57418a8dae889f5d
Tetiana-Kyryk-A <125405880+Tetiana-Kyryk-A@users.noreply.github.com>
2025-11-06 13:14:46 +0200

    Update Program.cs messages and product cycle limit (from 5 to 10)

--- Demo C# team project 2025/Program.cs
+++ Demo C# team project 2025/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, team!");
 Console.WriteLine("Go calc sum 1..5!");
-Console.WriteLine("Go calc product 1..5!");
+Console.WriteLine("Go calc product 1..10!");
 int sum = 0;
 for (int i = 1; i <= 5; i++)
 {
@@ -11,9 +11,11 @@ for (int i = 1; i <= 5; i++)
 Console.WriteLine("sum=" + sum);
 
 int product = 1;
-for (int i = 1; i <= 5; i++)
+for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"Count: {i}");
     product *= i;
 }
 Console.WriteLine(product);
+
+Console.WriteLine("Go calc average!")
