fcfea6adabb75c65213b488de486d4a48226954d
Tetyana-K <tanya.kyryk@gmail.com>
2025-11-06 13:42:49 +0200

    add feature average of 1..5

--- Demo C# team project 2025/Program.cs
+++ Demo C# team project 2025/Program.cs
@@ -18,4 +18,5 @@ for (int i = 1; i <= 10; i++)
 }
 Console.WriteLine(product);
 
-Console.WriteLine("Go calc average!")
+Console.WriteLine("Go calc average!");
+Console.WriteLine($"Average = {sum / 10.0}");
