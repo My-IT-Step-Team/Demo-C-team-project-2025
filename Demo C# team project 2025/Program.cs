715ac926bd651e639e264b46c5de36929b060185
Tetiana-Kyryk-A <125405880+Tetiana-Kyryk-A@users.noreply.github.com>
2025-11-06 13:01:47 +0200

    Update console writeline sum and add new message "Go calc product"

--- Demo C# team project 2025/Program.cs
+++ Demo C# team project 2025/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, team!");
 Console.WriteLine("Go calc sum 1..5!");
-
+Console.WriteLine("Go calc product 1..5!");
 int sum = 0;
 for (int i = 1; i <= 5; i++)
 {
     Console.WriteLine($"Count: {i}");
     sum += i;
 }
-Console.WriteLine(sum);
+Console.WriteLine("sum=" + sum);
 
 
